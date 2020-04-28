a35b0088219814b54a617d6ef7b44a640929fb51
Polykarpos <ptiftikoglou@gmail.com>
2020-04-28 17:22:10 +0300

    Added create ingredient feature.

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -10,7 +10,7 @@ class TagViewSet(viewsets.GenericViewSet, mixins.ListModelMixin, mixins.CreateMo
     authentication_classes = (TokenAuthentication,)
     permissions_classes = (IsAuthenticated,)
     queryset = Tag.objects.all()
-    serializers_class = serializers.TagSerializer
+    serializer_class = serializers.TagSerializer
 
     def get_queryset(self):
         """Return objects for the current authenticated user only"""
@@ -20,13 +20,17 @@ class TagViewSet(viewsets.GenericViewSet, mixins.ListModelMixin, mixins.CreateMo
         """Create  a new Tag"""
         serializer.save(user=self.request.user)
 
-class IngredientViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
+class IngredientViewSet(viewsets.GenericViewSet, mixins.ListModelMixin, mixins.CreateModelMixin):
     """Mange ingredients in the database"""
     authentication_classes = (TokenAuthentication,)
-    permission_classes = (IsAuthenticated,)
+    permissions_classes = (IsAuthenticated,)
     queryset = Ingredient.objects.all()
-    serializers_class = serializers.IngredientSerializer
+    serializer_class = serializers.IngredientSerializer
 
     def get_queryset(self):
         """Return objects for the current authenticated user"""
-        return  self.queryset.filter(user-self.request.user).oreder_by('-name')
+        return  self.queryset.filter(user=self.request.user).order_by('-name')
+
+    def perform_create(self, serializer):
+        """Crete a new ingredient"""
+        serializer.save(user=self.request.user)
